43efab486c62a9906cbe6f9705c847a8b78e3343
nekolr <excalibll@163.com>
2020-04-16 00:16:28 +0800

    Update github OAuth config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production'
-const clientId = isProduction ? '75cf00b02deb33e63424' : 'bf9d4f20def51dbd5c5c'
-const clientSecret = isProduction ? '6fa564cbd46f6bdfa1fb81ddce5503dcbe4ab4c4' : 'e8f5bc4369f64b05d052819047ca9d5e1d8b2e8f'
+const clientId = isProduction ? 'e4eade59975433c57dc0' : '65cb868a7f39130f09ac'
+const clientSecret = isProduction ? 'dc97c18949f327bb4bcac59d0c8f6bedc030c7ac' : '42336bf045880b746ed4b097a6ea56111cca13c1'
 
 export default {
   clientId,
